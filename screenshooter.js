7e347d233c0566c5128333926e86e6662702ef0d
Zachary Smith <billyzacsmith@gmail.com>
2016-03-19 10:35:30 -0600

    Initial commit.
    Take a screenshot of Flickr's home page and save it in a png.

---
+++ screenshooter.js
@@ -0,0 +1,23 @@
+var webshot = require('webshot')
+
+var options = {
+  screenSize: {
+    width: 1024,
+    height: 800
+  },
+  shotSize: {
+    width: 'window',
+    height: 'window'
+  },
+  shotOffset: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  }
+}
+
+var fileName = Date.now() + '.png'
+webshot('flickr.com', fileName, options, function(err) {
+  console.log('Image saved:', fileName)
+})
